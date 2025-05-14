94a2add009e2f999f7d9921cb0da8b5311fc1fca
davidsc21 <194646074+davidsc21@users.noreply.github.com>
2025-05-14 06:56:57 -0500

    doc: cambio styles de home

--- js/app.js
+++ js/app.js
@@ -4,7 +4,11 @@ const pages = {
     <audio src="media/audio/Awaken.mp3" autoplay loop></audio>
     <div class="containerhome">
           <div class="homecontent">
-            <p>hola</p>
+            <h1>BIENVENIDO INVOCADOR</h1>
+                <h2>En un mundo donde la magia y la guerra chocan sin tregua, solo los más valientes logran escribir su leyenda. Desde las sombras de Noxus hasta la armonía ancestral de Jonia, los campeones aguardan tu llamado.
+                    Cada carta es una decisión. Cada movimiento, un eco en el campo de batalla.
+                    ¿Tendrás el valor de enfrentarte al destino y forjar tu propia historia? 
+                    El juego ha comenzado, invocador.</h2>
             <div class="btnextra">
               <a href="#" onclick="navigate('arena')"><h2>COMIENZA TU LEYENDA</h2></a>
             </div>
@@ -42,3 +46,21 @@ function navigate(page) {
   content.innerHTML = pages[page] || '<h1>Página no encontrada</h1>';
 }
 
+
+//controlar modal/
+
+const elements = [
+  {
+    imagen: "../media/img/borralol.jpg"
+  },
+]
+function mostrarmodal(index){
+  document.getElementById("modal").style.display='block';
+  const troll = elements[index];
+
+  document.getElementById("modal-img").src=troll.imagen;
+}
+
+function cerrarmodal(){
+  document.getElementById("modal").style.display='none'
+}
